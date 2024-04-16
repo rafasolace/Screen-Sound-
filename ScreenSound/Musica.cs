8a9ed7f42955fd940a52f020370fea68e59c3519
Rafasolace <rafabropes4@gmail.com>
2024-04-16 16:04:40 -0300

    Adicionando programa e classe Musica

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,22 @@
+﻿class Musica //Essa classe serve para definir "dados e atributos" presentes nas variaveis Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool Disponivel { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome:{nome}");
+        Console.WriteLine($"Artista: {artista}"); //Esse void é o "método" 
+        Console.WriteLine($"Duração: {duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,18 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Moonlight";
+musica1.artista = "Ariana Grande";
+musica1.duracao = 202;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
+
+Musica musica2 = new Musica();
+musica2.nome = "Void";
+musica2.artista = "Melanie Marinez";
+musica1.duracao = 247;
+musica2.Disponivel = false;
+
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
